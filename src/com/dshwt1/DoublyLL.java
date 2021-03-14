3d0ce550459e0153e629bf87dc4ab1056f40922c
Makii117 <MakiOnPc@gmail.com>
2021-03-14 13:01:17 +0100

    Finished DLL

--- src/com/dshwt1/DoublyLL.java
+++ src/com/dshwt1/DoublyLL.java
@@ -25,50 +25,46 @@ public class DoublyLL<Item> implements Iterable<Item> {
     public void addToRear(Item item){
         Node<Item> newNode=new Node<Item>();
         newNode.data=item;
+        newNode.next=null;
+
         if(head==null){
-            head=newNode;
+            head=tail=newNode;
+            head.prev=tail.next=null;
         }
         else {
-            Node<Item> current=head;
-            while(current.next != null){
-                current=current.next;
-            }
-            current.next=newNode;
+        tail.next=newNode;
+        newNode.prev=tail;
+        tail=newNode;
+        tail.next=null;
         }
         size++;
     }
-    public void addAfter(Node<Item> oldNode,Item item){
-        Node<Item> newNode = new Node<Item>();
-        newNode.data=item;
 
-        newNode.next=oldNode.next;
-        oldNode.next=newNode;
-        newNode.prev=oldNode;
-        if(newNode.next!=null){
-            newNode.next.prev=newNode;
-        }
-    }
-
-    public void addBefore(Node<Item> oldNode, Item item){
-    Node<Item> newNode = new Node<Item>();
-    newNode.data=item;
-        if(newNode.prev == null){
-
-        }
-    }
 
 
 
     public void removeFromFront(){
         if(head==null){
-            throw new IndexOutOfBoundsException("LL is empty.");
+            throw new IndexOutOfBoundsException("List is empty.");
         }
         head=head.next;
         size--;
     }
 
     public void removeFromRear(){
-
+    if(head==null){
+        throw new IndexOutOfBoundsException("List is empty");
+        }
+    else{
+        if(head!=tail){
+            tail=tail.prev;
+            tail.next= null;
+        }
+        else {
+            head=tail=null;
+        }
+        }
+    size--;
     }
 
     public int count(){
@@ -79,13 +75,16 @@ public class DoublyLL<Item> implements Iterable<Item> {
         return new DoublyLLIterator();
     }
 
+
     private class DoublyLLIterator implements Iterator<Item>{
         Node<Item> current=head;
         public boolean hasNext(){
             return current != null;
         }
         public Item next(){
-
+        Item item= current.data;
+        current=current.next;
+        return item;
         }
 
     }
--- src/com/dshwt1/Main.java
+++ src/com/dshwt1/Main.java
@@ -3,6 +3,25 @@ package com.dshwt1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	DoublyLL<Integer> list = new DoublyLL<Integer>();
+
+
+	list.addToRear(1);
+	list.addToRear(3);
+	list.addToFront(4);
+	for(int i: list){
+        System.out.println(i);
     }
+		System.out.println("---------");
+	list.removeFromRear();
+	list.addToFront(7);
+		for(int i: list){
+			System.out.println(i);
+		}
+
+    list.removeFromFront();
+	list.addToRear(6);
+    }
+
+
 }
